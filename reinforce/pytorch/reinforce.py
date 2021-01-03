fc42fa171467f8bc34afea126ef6aef7d3f3a09c
chaddy <cpaik1004@gmail.com>
2021-01-03 15:29:05 +0900

    main function outline made for pt version

--- reinforce/pytorch/reinforce.py
+++ reinforce/pytorch/reinforce.py
@@ -40,6 +40,12 @@ class Reinforce:
         self.policy_network = Network(n_states=n_states, n_actions=n_actions)
         self.optim = Adam(params=self.policy_network.parameters(), lr=self.lr)
 
+    def reset(self):
+        self.states = []
+        self.actions = []
+        self.model_outputs = []
+        self.rewards = []
+
     def calculate_G(self):
         T = len(self.rewards)
         discounted_rewards = [0 for _ in range(T)]  # [G0, G2, G3, ... G_{T-1}]]
@@ -56,7 +62,7 @@ class Reinforce:
     def get_action(self, state):
         action_probs = self.policy_network(state)
         action_probs_np = action_probs.detach().cpu().numpy()
-        return np.random.choice(self.n_actions, 1, p=action_probs_np)
+        return np.random.choice(self.n_actions, 1, p=action_probs_np), action_probs
 
     def train_on_batch(self, states, targets):
         self.optim.zero_grad()
@@ -82,32 +88,21 @@ def main(episodes, exp_name):
         s_curr = s_curr.astype(np.float32)
         done = False
         score = 0
-        agent.update_weights()  # update weight every time an episode ends
+
         while not done:
             # if len(agent.experience_replay) == agent.replay_size:
             #     env.render()
             s_curr_tensor = torch.from_numpy(s_curr)
-            a_curr = agent.get_action(s_curr_tensor)
+            a_curr, model_output = agent.get_action(s_curr_tensor)
             s_next, r, done, _ = env.step(a_curr)
-            s_next_tensor = torch.from_numpy(s_next)
-            s_next = np.reshape(s_next, (1, states))
+            # s_next_tensor = torch.from_numpy(s_next)
+            # s_next = np.reshape(s_next, (1, states))
             r = r if not done or r > 499 else -100
-            sample = namedtuple('sample', ['s_curr', 'a_curr', 'reward', 's_next', 'done'])
 
-            sample.s_curr = s_curr_tensor
-            sample.a_curr = a_curr
-            sample.reward = r
-            sample.s_next = s_next_tensor
-            sample.done = done
-
-            if len(agent.experience_replay) < agent.replay_size:
-                agent.experience_replay.append(sample)
-                s_curr = s_next
-                continue
-            else:
-                agent.experience_replay.append(sample)
-                x_batch = random.sample(agent.experience_replay, agent.batch_size)
-                agent.train(x_batch)
+            agent.states.append(s_curr)
+            agent.actions.append(a_curr)
+            agent.rewards.append(r)
+            agent.model_outputs.append(model_output)
 
             score += r
 
@@ -116,6 +111,7 @@ def main(episodes, exp_name):
             if done:
                 score = score if score == 500 else score + 100
                 print(f"ep:{ep - warmup_ep}:################Goal Reached###################", score)
+                agent.train()
 
                 with writer.as_default():
                     tf.summary.scalar("reward", r, ep)
