a804a5bd2e58c39a17bb8c27594c27f974e83b99
chaddy <cpaik1004@gmail.com>
2021-01-08 13:24:51 +0900

    initialize pytorch a2c

---
+++ a2c/pytorch/a2c.py
@@ -0,0 +1,184 @@
+import argparse
+import os
+import random
+from collections import namedtuple, deque
+
+import gym
+import numpy as np
+import torch
+import tensorflow as tf
+from torch.nn import Module, Linear, ReLU, Sequential, Softmax
+from torch.nn import functional as F
+from torch.optim import Adam
+
+
+class Actor(Module):
+    def __init__(self, n_states, n_actions):
+        super(Actor, self).__init__()
+        self.lin1 = Sequential(Linear(in_features=n_states, out_features=16), ReLU())
+        self.lin2 = Sequential(Linear(in_features=16, out_features=24), ReLU())
+        self.final_lin = Sequential(Linear(in_features=24, out_features=n_actions), Softmax(dim=1))
+
+    def forward(self, x):
+        x = self.lin1(x)
+        x = self.lin2(x)
+        output = self.final_lin(x)
+        return output
+
+class Critic(Module):
+    def __init__(self, n_states, n_actions):
+        super(Critic, self).__init__()
+        self.lin1 = Sequential(Linear(in_features=n_states, out_features=16), ReLU())
+        self.lin2 = Sequential(Linear(in_features=16, out_features=24), ReLU())
+        self.final_lin = Sequential(Linear(in_features=24, out_features=n_actions), Softmax(dim=1))
+
+    def forward(self, x):
+        x = self.lin1(x)
+        x = self.lin2(x)
+        output = self.final_lin(x)
+        return output
+
+class A2C:
+    def __init__(self, n_states, n_actions):
+        self.states = []  # logging the states
+        self.actions = []  # logging the ACTUAL action that was performed at t
+        self.model_outputs = []  # logging PI(a|s) that was outputted at t
+        self.rewards = []  # logging the rewards that go from t_i -> t_{i+1}
+        self.n_actions = n_actions
+        self.lr = 0.001
+        self.batch_size = 64
+        self.gamma = 0.99
+        self.batch_indices = np.array([i for i in range(64)])
+        self.actor = Actor(n_states=n_states, n_actions=n_actions)
+        self.critic = Critic(n_states=n_states, n_actions=n_actions)
+        self.optim_actor = Adam(params=self.actor.parameters(), lr=self.lr)
+        self.optim_critic = Adam(params=self.critic.parameters(), lr=self.lr)
+
+    def reset(self):
+        self.states = []
+        self.actions = []
+        self.model_outputs = []
+        self.rewards = []
+
+    def calculate_G(self):
+        T = len(self.rewards)
+        discounted_rewards = [0 for _ in range(T)]  # [G0, G2, G3, ... G_{T-1}]]
+        last_index = T - 1
+        G_tp1 = 0  # G_{t+1}
+        for i, r in enumerate(reversed(self.rewards)):
+            # starting from {T-1}, counting down to to 0 (Given the episode started at 0 and ended at T-1)
+            curr_index = last_index - i
+            G_t = r + self.gamma * G_tp1
+            discounted_rewards[curr_index] = G_t
+            G_tp1 = G_t  # current G is the future G for the next iteration lol
+        # print(discounted_rewards)
+        discounted_rewards_tensor = torch.Tensor(discounted_rewards).unsqueeze(dim=1)
+        # print(self.rewards)
+
+        return discounted_rewards_tensor
+
+    def get_action(self, state):
+        action_probs = self.policy_network(state.float())
+        action_probs_np = action_probs.detach().cpu().numpy().squeeze()
+        return int(np.random.choice(self.n_actions, 1, p=action_probs_np)), action_probs
+
+    def get_action_probs(self):
+        model_outputs_tensor = torch.cat(self.model_outputs, 0)
+        index = torch.Tensor(self.actions).long()
+        chosen_actions_tensor = torch.zeros_like(model_outputs_tensor).long()
+        chosen_actions_tensor[torch.arange(chosen_actions_tensor.size(0)), index] = 1.
+        action_probs_tensor = model_outputs_tensor * chosen_actions_tensor
+        # print(model_outputs_tensor)
+        # print(action_probs_tensor)
+        action_probs_tensor_flattened = torch.sum(action_probs_tensor, dim=1).unsqueeze(1)
+        return action_probs_tensor_flattened
+
+    def train(self):
+        self.optim.zero_grad()
+        discounted_rewards = self.calculate_G()
+        action_probs = self.get_action_probs()
+        log_action_probs = torch.log(action_probs)
+        # print(log_action_probs.shape, discounted_rewards.shape)
+        cross_entropy = log_action_probs*discounted_rewards
+        # print(log_action_probs, discounted_rewards)
+        # print(cross_entropy)
+        loss = -1*torch.sum(cross_entropy)
+        loss.backward()
+        self.optim.step()
+        self.reset()
+        pass
+
+
+def main(episodes, exp_name):
+    logdir = os.path.join("logs", exp_name)
+    os.makedirs(logdir, exist_ok=True)
+    writer = tf.summary.create_file_writer(logdir)
+    env = gym.make('CartPole-v1')
+    states = env.observation_space.shape[0]  # shape returns a tuple
+    n_actions = env.action_space.n
+    agent = Reinforce(n_states=states, n_actions=n_actions)
+    warmup_ep = 0
+    for ep in range(episodes):
+        s_curr = np.reshape(env.reset(), (1, states))
+        s_curr = s_curr.astype(np.float32)
+        done = False
+        score = 0
+
+        while not done:
+            # if len(agent.experience_replay) == agent.replay_size:
+            #     env.render()
+            s_curr_tensor = torch.from_numpy(s_curr)
+            a_curr, model_output = agent.get_action(s_curr_tensor)
+            s_next, r, done, _ = env.step(a_curr)
+            # s_next_tensor = torch.from_numpy(s_next)
+            # s_next = np.reshape(s_next, (1, states))
+            r = r if not done or r > 499 else -100
+
+            agent.states.append(s_curr)
+            agent.actions.append(a_curr)
+            agent.rewards.append(r)
+            agent.model_outputs.append(model_output)
+
+            score += r
+
+            s_next = np.reshape(s_next, (1, states))
+            s_curr = s_next
+
+            if done:
+                score = score if score == 500 else score + 100
+                print(f"ep:{ep - warmup_ep}:################Goal Reached###################", score)
+                agent.train()
+
+                with writer.as_default():
+                    tf.summary.scalar("reward", r, ep)
+                    tf.summary.scalar("score", score, ep)
+    return agent
+
+
+def env_with_render(agent):
+    done = False
+    env = gym.make('CartPole-v1')
+    score = 0
+    states = env.observation_space.shape[0]  # shape returns a tuple
+    s_curr = np.reshape(env.reset(), (1, states))
+    while True:
+        if done:
+            score = 0
+            s_curr = np.reshape(env.reset(), (1, states))
+        env.render()
+        s_curr_tensor = torch.from_numpy(s_curr)
+        a_curr, _ = agent.get_action(s_curr_tensor)
+        s_next, r, done, _ = env.step(a_curr)
+        s_next = np.reshape(s_next, (1, states))
+        s_curr = s_next
+        score += r
+        print(score)
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--exp_name", type=str, default="PG_REINFORCE_pt", help="exp_name")
+    ap.add_argument("--episodes", type=int, default=1300, help="number of episodes to run")
+    args = vars(ap.parse_args())
+    trained_agent = main(episodes=args["episodes"], exp_name=args["exp_name"])
+    env_with_render(agent=trained_agent)
